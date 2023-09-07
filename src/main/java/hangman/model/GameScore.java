8e7b60c31b7aa425c4272afabb2295bc7fb16d16
ChristianDuarteR <christianjavierduarte@gmail.com>
2023-09-07 16:40:08 -0500

    borrando master

--- src/main/java/hangman/model/GameScore.java
+++ src/main/java/hangman/model/GameScore.java
@@ -3,9 +3,11 @@ package hangman.model;
 public interface GameScore {
 
     /**
-     * @param CorrectCount numero de letras correctas
+     * @param CorrectCount   numero de letras correctas
      * @param IncorrectCount numero de letras incorrectas
      * @return score
      */
-    int CalculateScore(int CorrectCount, int IncorrectCount) throws GameScoreException;
+    default int CalculateScore(int CorrectCount, int IncorrectCount) throws GameScoreException {
+        return 0;
+    }
 }
